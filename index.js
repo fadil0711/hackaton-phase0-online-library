e7178925a1457b462fee4589e6f92a85de687e3f
“fadil0711” <outher.heaven07@gmail.com>
2026-07-23 16:44:27 +0700

    fixed login to admin

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-// Mengambil elemen tombol login dari HTML
 const loginBtn = document.getElementById('loginBtn');
 
-// Menambahkan event listener ketika tombol login diklik
 loginBtn.addEventListener('click', function () {
-    // Mengambil nilai input dari elemen email dan password
+
     const emailInput = document.getElementById('email').value;
     const passwordInput = document.getElementById('password').value;
 
-    // Kondisi pengecekan
-    if (emailInput === 'admin' && passwordInput === 'admin') {
-        // Pindah ke halaman admin.html
+    if (emailInput === 'admin' && passwordInput === '!Admin07') {
+
         window.location.href = 'admin.html';
+
     } else {
-        // Tampilkan peringatan jika tidak sesuai
-        alert('email atau password yang anda masukan salah!');
+
+        Swal.fire({
+            icon: 'error',
+            title: 'Login Gagal',
+            text: 'Email atau password yang Anda masukkan salah!',
+            confirmButtonText: 'OK'
+        });
+
     }
+
 });
